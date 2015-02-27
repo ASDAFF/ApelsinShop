fbb987052722846dcf91356f2735fefb85be8b67
Olga Rjabchikova <olga.rjabchicova@gmail.com>
2015-02-27 10:45:20 +0300

    Обновление

---
+++ components/Shop/admin/elementsUI/shopImportXMLtoMYSQL.php
@@ -0,0 +1,6 @@
+<?php
+include_once './components/Shop/admin/classes/ShopImportData.php';
+include_once './components/Shop/admin/classes/ShopImportData_MySQL.php';
+include_once './components/Shop/admin/classes/ShopImportData_XML.php';
+//$shopImportData = new ShopImportData();
+//echo $shopImportData->getReport();
