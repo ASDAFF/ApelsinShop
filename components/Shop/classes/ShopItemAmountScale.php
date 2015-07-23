dc688389c67b47404634b97b10969444535c218c
Maxim Zaytsev <zaytsev.max90@gmail.com>
2015-07-23 10:29:00 +0300

    APLS-44: Количество товаров подсвечивается разным цветом

--- components/Shop/classes/ShopItemAmountScale.php
+++ components/Shop/classes/ShopItemAmountScale.php
@@ -7,21 +7,26 @@ class ShopItemAmountScale {
         if($amount <= 0 || $amount == 1) {
             $scaleValue = 0;
             $text = "Товара нет в наличии.";
+            $css = "";
         } else if ($amount >= 0 && $amount < $minAmount) {
             $scaleValue = 25;
             $text = "Очень мало.\nУточняйте наличие в магазине у менеджеров.";
+            $css = "VeryFew";
         } else if ($amount >= $minAmount && $amount < 2 * $minAmount) {
             $scaleValue = 50;
             $text = "Мало.";
+            $css = "Few";
         } else if ($amount >= 2 * $minAmount && $amount < 3 * $minAmount) {
             $scaleValue = 75;
             $text = "Достаточно.";
+            $css = "Suffice";
         } else if ($amount >= 3 * $minAmount) {
             $scaleValue = 100;
             $text = "Много.";
+            $css = "Many";
         }
         $out = "<div class='ShopItemAmount' title='".$text."'>";
-        $out .= "<div class='ShopItemAmountScale' style='width: ".$scaleValue."%;'></div>";
+        $out .= "<div class='ShopItemAmountScale ".$css."' style='width: ".$scaleValue."%;'></div>";
         $out .= "</div>";
         return $out;
     }
