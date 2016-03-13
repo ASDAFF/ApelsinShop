3e26b5e91a42498b835925e77d6e6c53db1b80c9
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-03-13 16:24:22 +0300

    Изменение кнопок "каталог" и "фильтры" и добавление кнопки "назад" в карточке товара.

--- modules/ShopNavigationAndFiltersPanelMenu/index.php
+++ modules/ShopNavigationAndFiltersPanelMenu/index.php
@@ -4,19 +4,20 @@ $page = $_URL_PARAMS['page'];
 $params = $_URL_PARAMS['params'];
 echo "<div class='ShopNavigationAndFiltersPanelMenuMainBlock'>";
     
-if (
-        $page == 'shop' && 
-        (
-                (!isset($params[0])) || 
-                (isset($params[0]) && $params[0] === ShopPageInfoHelper::getCatalogPage()) || 
-                (isset($params[0]) && $params[0] === 'p')
-        )
-    ) {
-    echo "<div class='ShopNavigationAndFiltersPanelMenuBlock'>";
-    echo "<div class='ShopNavigationAndFiltersPanelMenuElement meShopFilterBlock' blockId='ShopFilterBlock'>фильтры <span class='ShopFilterBlockCloseButton'>X</span></div>";
-    echo "<div class='ShopNavigationAndFiltersPanelMenuElement meShopNavigationMenuBlock' blockId='ShopNavigationMenuBlock'>навигация <span class='ShopFilterBlockCloseButton'>X</span></div>";
-    echo "</div>";
-}
+if ($page == 'shop') {
+    if(!isset($params[0])|| (isset($params[0]) && $params[0] === ShopPageInfoHelper::getCatalogPage())|| (isset($params[0]) && $params[0] === 'p')) {
+        echo "<div class='ShopNavigationAndFiltersPanelMenuBlock'>";
+        echo "<div class='ShopNavigationAndFiltersPanelMenuElement meShopNavigationMenuBlock' blockId='ShopNavigationMenuBlock'>каталог <span class='ShopFilterBlockCloseButton'>X</span></div>";
+        echo "<div class='ShopNavigationAndFiltersPanelMenuElement meShopFilterBlock' blockId='ShopFilterBlock'>фильтры <span class='ShopFilterBlockCloseButton'>X</span></div>";
+        echo "</div>";
+    } else if(isset($params[0]) && $params[0] === ShopPageInfoHelper::getItemPage()) {
+        echo "<div class='ShopNavigationAndFiltersPanelMenuBlock ItemBack'>";
+//        echo "<div class='BackOutFromItemPage' onclick='history.back();'>На предыдущую страницу</div>";
+        echo "<div class='BackOutFromItemPage' onclick='history.back();'>Назад</div>";
+//        echo "<div class='ShopNavigationAndFiltersPanelMenuElement' onclick='history.back();'>В корень</div>";
+        echo "</div>";
+    }
+} 
     include_once './components/Shop/classes/ShopIncludeClasses.php';
     ShopIncludeClasses::includeAllClasses();
     echo ShopGroupsPathPanelHelper::getPanel();
